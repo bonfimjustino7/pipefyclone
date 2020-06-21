04963089e5fefbe4b1003d51239961ac19e8d41b
Bonfim Justino do Nascimento <bonfim1999@gmail.com>
2020-06-21 18:08:03 -0300

    Implementando funcionalidade Drag in drop

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React from "react";
 import GlovalStyle from "./styles/global";
 import Header from "./components/Header";
 import Board from "./components/Board";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Header />
       <Board />
       <GlovalStyle />
-    </>
+    </DndProvider>
   );
 }
 
