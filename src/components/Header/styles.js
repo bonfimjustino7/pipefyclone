891f7cebe2b385d0aedf93b8e69ad531ef9c0131
Bonfim Justino do Nascimento <bonfim1999@gmail.com>
2020-06-21 16:22:39 -0300

    Aplicação sem drag in drop

---
+++ src/components/Header/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 80px;
+  padding: 0 30px;
+  background: #4168fa;
+  color: #fff;
+
+  display: flex;
+  align-items: center;
+`;
